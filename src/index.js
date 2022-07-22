97a1658237d48ec6114b56e7d8b432a956103bb3
Reid Efford <36901822+effordDev@users.noreply.github.com>
2022-07-22 13:01:36 +0000

    misc

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('', (req, res) => {
-    console.log(req) //see what the request looks like
+    //console.log(req) //see what the request looks like
 
     res.send('sent from node :)')
     // res.render()
 
 })
 
-app.get('/test/:id', (req, res) => {
+app.get('/message/:message', (req, res) => {
 
-    const _id = req.params.id
-    console.log(_id) //see what the request ID looks like
+    const _message = req.params.message
+    console.log(_message) //see what the request ID looks like
 
-    if(req.params.id.length > 5) {
-        res.send('The id sent is: ' + req.params.id)
+    if(_message) {
+        res.send('The _message sent is: ' + _message)
     } else {
-        console.log('no ID provided, returning 404')
+        console.log('no _message provided, returning 404')
         res.status(404).send()
     }
 
